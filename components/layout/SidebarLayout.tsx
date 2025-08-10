366a963ae1bac1733559caedbc3d4308887010df
gukt <29283212@qq.com>
2025-08-10 08:59:29 +0800

    udpates

--- components/layout/SidebarLayout.tsx
+++ components/layout/SidebarLayout.tsx
@@ -1,13 +1,12 @@
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { Sidebar2 } from './Sidebar2';
+import { Sidebar } from './Sidebar';
 
 export function SidebarLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider defaultOpen={true}>
-      <div className="grid grid-cols-[16rem_1fr] gap-6">
-        <Sidebar2 />
-        <div className="flex-1 px-6 pt-2 pb-8 overflow-y-auto">{children}</div>
-      </div>
-    </SidebarProvider>
+    <div className="grid grid-cols-[16rem_1fr] gap-6">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Right Content */}
+      <div className="flex-1 px-6 pt-2 pb-8 overflow-y-auto">{children}</div>
+    </div>
   );
 }
